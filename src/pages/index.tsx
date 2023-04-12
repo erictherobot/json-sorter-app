2b1c888aa8a084c3d2a72d9dd779e0d9e75d2827
Eric David Smith <erictherobot@gmail.com>
2023-04-12 17:31:30 -0400

    Adding a touch of SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import JsonInput from "@/components/JsonInput";
 import JsonOutput from "@/components/JsonOutput";
 import SortOptions from "@/components/SortOptions";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 
 export default function Home() {
   const [jsonInput, setJsonInput] = useState("");
@@ -13,12 +14,22 @@ export default function Home() {
   };
 
   return (
-    <div className="container mx-auto py-4 px-4">
-      <h1 className="text-4xl font-bold mb-4 text-white">JSON Sorter</h1>
-      <JsonInput onJsonChange={setJsonInput} />
-      <SortOptions onSort={handleSort} />
-      <JsonOutput inputJson={jsonInput} sortOptions={sortOptions} />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>JSON Sorter Online - Sort JSON Easily - Dev Utilities</title>
+        <meta
+          name="description"
+          content="Looking to sort your JSON data? This free online JSON sorter will sort your JSON data in alphabetical order, and will also remove any duplicate keys. It's easy to use, and it's free!"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto py-4 px-4">
+        <h1 className="text-4xl font-bold mb-4 text-white">JSON Sorter</h1>
+        <JsonInput onJsonChange={setJsonInput} />
+        <SortOptions onSort={handleSort} />
+        <JsonOutput inputJson={jsonInput} sortOptions={sortOptions} />
+        <Footer />
+      </div>
+    </>
   );
 }
