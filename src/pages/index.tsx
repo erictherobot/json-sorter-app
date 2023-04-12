2481d8f184a70099dffb03b3b799fefb55e297fb
Eric David Smith <erictherobot@gmail.com>
2023-04-12 16:44:06 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   };
 
   return (
-    <div className="container mx-auto py-4">
+    <div className="container mx-auto py-4 px-4">
       <h1 className="text-4xl font-bold mb-4 text-white">JSON Sorter</h1>
       <JsonInput onJsonChange={setJsonInput} />
       <SortOptions onSort={handleSort} />
